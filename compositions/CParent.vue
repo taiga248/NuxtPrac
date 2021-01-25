ded963e686bb927fc873be11563b1e601166995f
t.ito <t.ito@sonicmoov.com>
2021-01-25 18:55:16 +0900

    Add: composition api周り

---
+++ compositions/CParent.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h2>{{ title }}</h2>
+    <p>count : {{ state.count }}</p>
+    <CChild :title="childTitle" @countUp="clickEvent" />
+  </div>
+</template>
+
+<script lang="ts">
+import { reactive } from '@vue/composition-api'
+import CChild from '@/compositions/CChild.vue'
+
+export default {
+  components: { CChild },
+  setup() {
+    // 値をリアクティブに
+    const title = 'Parent Title'
+    const childTitle = 'Child Title'
+    const state = reactive({
+      count: 123,
+    })
+    function clickEvent(): number {
+      return state.count++
+    }
+    return { title, childTitle, state, clickEvent }
+  },
+}
+</script>
