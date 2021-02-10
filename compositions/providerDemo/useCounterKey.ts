154c1036e8ff43d5947a149dde5734c1830825e6
t.ito <t.ito@sonicmoov.com>
2021-02-10 19:02:03 +0900

    ファイル名変更, 諸々

--- compositions/providerDemo/useCounterKey.ts
+++
@@ -1,5 +0,0 @@
-import { InjectionKey } from '@vue/composition-api'
-import { CounterStore } from '@/compositions/providerDemo/useCounter'
-
-const CounterKey: InjectionKey<CounterStore> = Symbol('CounterStore')
-export default CounterKey
--- pages/compositions.vue
+++ pages/compositions.vue
@@ -6,17 +6,13 @@
     <v-col cols="12">
       <Composition />
     </v-col>
-    <v-col cols="12">
-      <PParent />
-    </v-col>
   </v-row>
 </template>
 
 <script lang="ts">
 import Composition from '@/components/Compos.vue'
-import PParent from '@/components/providerDemo/PParent.vue'
 
 export default {
-  components: { Composition, PParent },
+  components: { Composition },
 }
 </script>
